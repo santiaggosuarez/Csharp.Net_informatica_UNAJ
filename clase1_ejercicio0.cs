0b70b8fbcec6b1f819015216e53ca04b2b6f71d0
Santiago Suárez <santiaggosuarez@gmail.com>
2022-08-25 08:31:45 -0300

    Update clase1_ejercicio0.cs

--- clase1_ejercicio0.cs
+++ clase1_ejercicio0.cs
@@ -14,19 +14,25 @@ namespace Ejercicio1
 	{
 		public static void Main(string[] args)
 		{	
-			// De una secuencia de números ingresado por teclado, imprimir el máximo y mínimo. Pueden usar el 0 para cortar.
-			int num, max = 0, min = 0;
+			# De una secuencia de números ingresado por teclado, imprimir el máximo y mínimo. Pueden usar el 0 para cortar.
+			# Utilizando la misma lógica que en Python, pero programado en C#
 			
-			Console.Write("Ingrese un número: ");
+			# Declaración de las variables a utilizar
+			int num, max, min;
+			
+			Console.Write("Ingrese un número (´0´ para finalizar): ");
 			num = int.Parse(Console.ReadLine());
+			
+			# Se le da a max y min el valor del primer número ingresado
+			# Esto permite que el progama funcione aún si se ingresa un número exagareadamene alto o bajo
 			max = num;
 			min = num;
 			
 			while (num != 0){
 				if (num > max ){
 					max = num;
-				} 
-				if (num < min){
+				}
+				else if (num < min){
 					min = num;
 				}
 				
@@ -34,7 +40,7 @@ namespace Ejercicio1
 				num = int.Parse(Console.ReadLine());
 			}
 			
-			Console.WriteLine("El numero mayor es {0} y el menor es {1}", max, min);
+			Console.WriteLine("\nEl numero mayor es {0} y el menor es {1}", max, min);
 			
 			
 			Console.Write("\nPress any key to continue . . . ");
