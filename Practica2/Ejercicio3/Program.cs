e039b66cd213a220d7def646bfbe4efa6376121d
Santiago Suárez <santiaggosuarez@gmail.com>
2022-09-05 22:49:32 -0300

    Add files via upload

---
+++ Practica2/Ejercicio3/Program.cs
@@ -0,0 +1,61 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Santiago
+ * Date: 2/9/2022
+ * Time: 12:58 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections;
+
+namespace Practica2
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			// 3- Escriba un programa de aplicación que lea 2 palabras y determine si son palíndromos entre ellas. (Ej: “abbccd” y “dccbba” son palíndromos). 
+			
+			Console.WriteLine("Ingrese dos palabras a comparar: ");
+			String pal1 = Console.ReadLine();
+			String pal2 = Console.ReadLine();
+			
+			pal1 = pal1.ToLower();
+			pal2 = pal2.ToLower();
+			
+			bool palind;
+							
+			if (pal1.Length  == pal2.Length){
+				
+				palind = true;
+				int largo_pal2 = pal2.Length;
+				int j = largo_pal2 -1;
+				
+					for (int i = 0; i < largo_pal2; i++ ){
+					if (pal1[i] != pal2[j]) {
+						palind = false;
+						break;
+					}
+					else {
+						j = j -1;
+						palind = true;
+					}
+				}
+ 			}
+			else {
+				palind = false;
+			}
+			
+			if (palind){
+				Console.WriteLine("\nES palindromo.");
+			}
+			else {
+				Console.WriteLine("\nNO es palindromo.");
+			}
+			
+			Console.Write("\nPress any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
---
+++ Practica2/Ejercicio7/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Santiago
+ * Date: 5/9/2022
+ * Time: 8:14 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+/*
+ * Created by SharpDevelop.
+ * User: Santiago
+ * Date: 5/9/2022
+ * Time: 8:14 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Ejercicio7
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			// Implemente una función que reciba una matriz (arreglo de dos dimensiones)
+			// pasada como parámetro, e imprima por consola la suma de sus elementos
+
+      // Creación de la matriz (su suma da 78)
+			double [,] matriz = new double[,] {{ 1, 2, 3, 4 },{ 5, 6, 7, 8 },{ 9, 10, 11, 12 }};
+			
+			Console.WriteLine("Suma total de los elementos: " + sumaMatriz(matriz));
+			
+			Console.Write("\nPress any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+		
+		public static double sumaMatriz(double [,] mat){
+			double suma = 0;
+      // Con get length obtenemos el largo de cada dimensión (0,1)
+			int row = mat.GetLength(0);
+			int column = mat.GetLength(1);
+      // Primero se selecciona la fila (i) y con el for anidado se recorre la columna (k) dentro de ella
+			for(int i = 0; i < row; i++){
+				for (int k = 0; k < column; k++){
+					suma += mat[i,k];
+				}
+			}
+			return suma;
+		}
+	}
+}
