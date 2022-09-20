83ad14c5aaa6d0181d59d37b4804e9665d818707
Santiago Suárez <santiaggosuarez@gmail.com>
2022-09-20 09:17:46 -0300

    Add files via upload

---
+++ Ej_1_2_3_4_5/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Santiago
+ * Date: 20/9/2022
+ * Time: 8:02 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Ej_1_2_3_4_5
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
