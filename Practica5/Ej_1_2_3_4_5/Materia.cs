ad43a09fe98db37d335e61ead15ae4c1bf5d3487
Santiago Suárez <santiaggosuarez@gmail.com>
2022-09-22 08:12:27 -0300

    Add files via upload

---
+++ Practica5/Ej_1_2_3_4_5/Materia.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Santiago
+ * Date: 21/9/2022
+ * Time: 9:08 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections;
+
+namespace Ej_1_2_3_4_5
+{
+	// 4) Implemente la clase Materia que permita asociarle el profesor y la lista de los alumnos inscriptos en ella.
+	
+	/// <summary>
+	/// Description of Materia.
+	/// </summary>
+	public class Materia
+	{
+		// Atributos
+		string nombreMateria;
+		Profesor profe;
+		ArrayList listaAlumnos;
+		
+		// Constructores
+		public Materia()
+		{
+		}
+		public Materia(string nombre, Profesor profe, ArrayList listaAlumnos)
+		{
+			this.nombreMateria = nombre;
+			this.profe = profe;
+			this.listaAlumnos = listaAlumnos;
+		}
+		
+		// Propiedades
+		public string NombreMateria
+		{
+			set {nombreMateria = value;}
+			get {return nombreMateria;}
+		}
+		public Profesor Profe
+		{
+			set {profe = value;}
+			get {return profe;}
+		}
+		public ArrayList ListaAlumnos
+		{
+			set {listaAlumnos = value;}
+			get {return listaAlumnos;}
+		}
+	}
+}
---
+++ Practica5/Ej_1_2_3_4_5/Universidad.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Santiago
+ * Date: 21/9/2022
+ * Time: 7:29 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections;
+
+namespace Ej_1_2_3_4_5
+{
+	/*5) Utilizando las clases definidas en los ejercicios anteriores implemente la clase Universidad que tiene
+		su conjunto de profesores, un conjunto de materias y un conjunto de alumnos. Entre su
+		funcionalidad debe permitar:
+		a) La inscripción de alumnos a materias
+		b) La asignación de profesores a las materias
+		c) Listar el profesor y los alumnos de una determinada materia
+		*/
+		
+	/// <summary>
+	/// Description of Universidad.
+	/// </summary>
+	public class Universidad
+	{
+		// Atributos
+		ArrayList profesores, alumnos, materias;
+		
+		// Constructores
+		public Universidad()
+		{
+			profesores = new ArrayList();
+			alumnos = new ArrayList();
+			materias = new ArrayList();;
+		}
+		
+		// Propiedades
+		public ArrayList Profesores
+		{
+			get {return profesores;}
+		}
+		public ArrayList Alumnos
+		{
+			get {return alumnos;}
+		}
+		public ArrayList Materias
+		{
+			get {return materias;}
+		}
+	}
+}
